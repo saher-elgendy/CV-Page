e42cca75487d30ce596912446b17e54b775ee1f6
unknown <saherelgendy1990@gmail.com>
2019-02-21 11:19:01 +0200

    refactor gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,6 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default', () => {
-    console.log('gulp is running')
-})
- 
 
 let autoprefixBrowsers = ['> 1%', 'last 2 versions', 'firefox >= 4', 'safari 7', 'safari 8', 'IE 8', 'IE 9', 'IE 10', 'IE 11'];
 
