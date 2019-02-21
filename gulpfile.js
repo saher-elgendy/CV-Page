3aa3cd720bea589a2066160a7aae3973288112ab
unknown <saherelgendy1990@gmail.com>
2019-02-21 15:24:00 +0200

    making the dropdown menu responsive

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,10 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 
-
 let autoprefixBrowsers = ['> 1%', 'last 2 versions', 'firefox >= 4', 'safari 7', 'safari 8', 'IE 8', 'IE 9', 'IE 10', 'IE 11'];
 
 gulp.task('prefix', function () {
-    gulp.src('src/app.css')
+    return gulp.src('src/app.css')
     .pipe(autoprefixer({ browsers: autoprefixBrowsers }))
     .pipe(gulp.dest('styles'));
 });
